66104f09ba1f567bf1c7e5b788eb2e1fb009594c
Liam Daniel Hurt <liamhurt51@gmail.com>
2017-09-28 16:58:23 -0700

    My first unity commit and i build a text damage indicator that when you click the block it damages it but the block heals its self

---
+++ Assets/Scripts/DamageTextAnimation.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTextAnimation : MonoBehaviour {
+    public int startFontSize = 40;
+    public int endFontSize = 55;
+    public int fontSizeVar = 10;
+    public float animStartY = 1.5f;
+    public float animEndY = 3f;
+    public float animVar = 0.2f;
+    public float animSpeed = 1.0f;
+    public Color textColor = new Color(255, 0, 0, 0);
+
+    bool bAnimate = false;
+    float t = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (bAnimate == true) {
+            AnimateDamageText();
+            t += animSpeed * Time.deltaTime;
+            if (t > animSpeed) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void SetDamageText(int amount) {
+        Debug.Log("SetText");
+        ClearDamageText();
+        GetComponent<TextMesh>().text = amount.ToString();
+        bAnimate = true;
+    }
+
+    private void ClearDamageText() {
+        t = 0.0f;
+        bAnimate = false;
+        animStartY += Random.Range(-animVar, animVar);
+        animEndY += Random.Range(-animVar, animVar);
+        GetComponent<TextMesh>().text = "";
+        GetComponent<TextMesh>().fontSize = startFontSize += Random.Range(-fontSizeVar, fontSizeVar);
+        transform.position = new Vector3(transform.position.x + Random.Range(-animVar, animVar), animStartY, transform.position.z + Random.Range(-animVar, animVar));
+        GetComponent<TextMesh>().color = textColor;
+    }
+
+    private void AnimateDamageText() {
+        transform.position = new Vector3(transform.position.x, Mathf.Lerp(animStartY, animEndY, t), transform.position.z);
+        GetComponent<TextMesh>().fontSize = (int)Mathf.Lerp(startFontSize, endFontSize, t);
+        GetComponent<TextMesh>().color = new Color(GetComponent<TextMesh>().color.r, GetComponent<TextMesh>().color.g, GetComponent<TextMesh>().color.b, Mathf.Lerp(1, 0, t));
+    }
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+    public float maxHealth = 100.0f;
+    public float curHealth = 0.0f;
+    public GameObject healthBar;
+
+	// Use this for initialization
+	void Start () {
+        curHealth = maxHealth;
+        InvokeRepeating("IncreaseHealth", 1.0f, 1.0f);
+	}
+
+    void OnMouseDown() {
+        DecreaseHealth(Random.Range(1, 10));
+    }
+
+    void DecreaseHealth(int amount) {
+        if (curHealth == 0.0f) {
+            Debug.Log("Player is dead");
+            Destroy(gameObject);
+        }
+        else if (curHealth > 0.0f) {
+            curHealth = Mathf.Clamp(curHealth - amount, 0.0f, maxHealth);
+            CreateDamageText(amount);
+        }
+        float calcHealth = curHealth / maxHealth;
+        SetHealthBar(calcHealth);
+    }
+
+    void IncreaseHealth() {
+        if (curHealth == maxHealth) {
+            Debug.Log("Player full health");
+        }
+        else if (curHealth < maxHealth) {
+            curHealth = Mathf.Clamp(curHealth + 5.0f, 0.0f, maxHealth);
+        }
+        float calcHealth = curHealth / maxHealth;
+        SetHealthBar(calcHealth);
+    }
+
+    public void SetHealthBar(float myHealth) {
+        healthBar.transform.localScale = new Vector3(myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
+
+    public void CreateDamageText(int amount) {
+        var instText = new GameObject("DamageText");
+        var textMesh = instText.AddComponent<TextMesh>();
+        var faceCam = instText.AddComponent<FaceCamera_Late>();
+        var anim = instText.AddComponent<DamageTextAnimation>();
+
+        instText.transform.parent = transform;
+        faceCam.reverseFace = true;
+        textMesh.characterSize = 0.1f;
+        textMesh.anchor = TextAnchor.MiddleCenter;
+        textMesh.alignment = TextAlignment.Center;
+        textMesh.fontStyle = FontStyle.Bold;
+        anim.SetDamageText(amount);
+    }
+}
